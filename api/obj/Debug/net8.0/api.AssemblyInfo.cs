edd0f303bd816ef91abd1e63e8f8fd6b99e15f60
Erin Kiley <erinkiley@Erins-MacBook-Pro.local>
2025-04-11 13:32:10 -0500

    erins

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c64225ff089a5222117a7c4baba3c27349d18ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b5b559d3b88c3d3af347651f4fc61b72ae585f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
